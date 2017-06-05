cb2473fa154f4dd85242488376932a7b2f347759
ErhanCin <ErhanCin@gmx.de>
2017-06-05 15:50:53 +0200

    Add files via upload

---
+++ ArrayErstellen.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ArrayErstellen {
+	public static void main(String[] args){
+  		String[] namen = {"Peter", "Hans","Jürgen","Lisa","Anna"};
+		
+		System.out.println(Arrays.toString(namen));
+	}
+
+
+}
